60984356a12f3daaafc2d1450dc84a44a2999851
Chris Samuel <Chris Samuel@DESKTOP-U0V2R58>
2020-06-06 03:03:43 +0100

    second task

--- src/tasktwo/TaskTwo.java
+++ src/tasktwo/TaskTwo.java
@@ -21,14 +21,14 @@ public class TaskTwo {
         String language;
         
           
-          firstName = "Christian";
-          lastName = "Samuel";
-          language = "Java";
+          firstName = "[Christian]";
+          lastName = "[Samuel]";
+          language = "[Java]";
           
           System.out.print("Hello World, this is");
           System.out.print(" "+ firstName);
           System.out.println(" "+ lastName);
-          System.out.println("With HNGi7 ID [06727]");
+          System.out.println("With HNGi7 [06727]");
           System.out.println("Using " + language + " for stage 2 task ");
           System.out.println("crismiles@gmail.com");
         
